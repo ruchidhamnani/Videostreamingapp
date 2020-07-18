e4946576cfbdebdec5558057f7970fbc2e01a344
ruchidhamnani <dhamnaniruchi.2909@gmail.com>
2020-07-18 13:51:04 +0530

    Adding playlist option

--- script.js
+++ script.js
@@ -17,29 +17,42 @@ $(document).ready(function(){
 	
 	
 	})
+
+
 	
 	function videoSearch(key, search, maxResults){
 		$("#bigvideoplayer").empty()
 		$("#videos").empty()
 		$("#unorderedlist").empty()
 		
+		
 		$.get("https://www.googleapis.com/youtube/v3/search?key="+ key + "&type=video&part=snippet&maxResults=" +maxResults + "&q=" + search,function(data){
 			console.log(data)
 			
 			data.items.forEach(item=>{
 				videoId = item.id.videoId
+				
 				$.get("https://www.googleapis.com/youtube/v3/videos?part=snippet&id=" + videoId + "&fields=items/snippet/title,items/snippet/description,items&key=" +key,function(abc){
-					
-					abc.items.forEach(item=>{listitem = `<li id= ${item.id} >${item.snippet.title}</li> 
-					<button onclick= "funcplaylist()">Add to playlist</button>`
+					var buttonid = "addtofav-"+String(item.id)
+					abc.items.forEach(item=>{listitem = `<li id= ${item.id} >${item.snippet.title}
+						
+					</li> 
+					<button id= "${buttonid}">Add to playlist</button>
+					`
 					$("#unorderedlist").append(listitem) 
+					$(document).ready(function(){
+						document.getElementById(buttonid).click(function(){
+							alert("Hello World");   
+						});
+					  });
 					// document.getElementById("button-"+item.id).addEventListener("click",function(e){
 						
 					// })
-					function funcplaylist(){
-						litem = `<li>constant</li>`
-									$("#listofplaylist").append(litem)
-					}
+					// function funcplaylist(){
+					// 	console.log("====================")
+					// 	litem = `<li>constant</li>`
+					// 				$("#listofplaylist").append(litem)
+					// }
 					document.getElementById(item.id).addEventListener("click",function(e){
 						$("#bigvideoplayer").empty()
 						bigvideo= `<iframe width="720" height="515" src="http://www.youtube.com/embed/${e.target.id}" frameborder="0" allowfullscreen></iframe>`
